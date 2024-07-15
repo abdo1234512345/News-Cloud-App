067182520caa0af1f63b614577cd2091168f0ebf
abdo1234512345 <41443491+abdo1234512345@users.noreply.github.com>
2024-07-15 18:28:19 +0200

    NEWS Cloud App

---
+++ lib/services/news_service.dart
@@ -0,0 +1,34 @@
+import 'package:dio/dio.dart';
+import 'package:news_app_ui_setup/models/article_model.dart';
+
+class NewsService {
+  final Dio dio;
+
+  NewsService(this.dio);
+
+  Future<List<ArticleModel>> getTopHeadlines({required String category}) async {
+    try {
+      var response = await dio.get(
+          'https://newsapi.org/v2/top-headlines?q=trump&apiKey=3732c8b18be0482080a796a2e0d2ac03');
+
+      Map<String, dynamic> jsonData = response.data;
+
+      List<dynamic> articles = jsonData['articles'];
+
+      List<ArticleModel> articlesList = [];
+
+      for (var article in articles) {
+        ArticleModel articleModel = ArticleModel(
+          image: article['urlToImage'],
+          title: article['title'],
+          subTitle: article['description'],
+        );
+        articlesList.add(articleModel);
+      }
+
+      return articlesList;
+    } catch (e) {
+      return [];
+    }
+  }
+}
